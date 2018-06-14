a5359d86c944a094c2f3ffdb600e7ffa9a99e917
Agata Czerwińska <agata@rubrica.pl>
2018-06-14 11:25:41 +0200

    Add loading icon in BookDetails Component

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import BookSelectButton from './BookSelectButton'
 import * as BooksAPI from './BooksAPI'
+import LoadingIcon from './icons/loading-icon.gif'
 
 
 class BookDetails extends Component {
 
     state = {
-        bookDetails: {}
+        bookDetails: {},
+        isLoaded: false
     }
 
     componentDidMount() {
             const id = this.props.match.params.bookId
             BooksAPI.get(id).then((bookDetails) => {
                 this.setState({
-                bookDetails: bookDetails
+                bookDetails: bookDetails,
+                isLoaded: true
                 })
             })
     }
@@ -22,6 +25,7 @@ class BookDetails extends Component {
     render() {
         const { onUpdate } = this.props
         const bookDetails = this.state.bookDetails
+        const isLoaded = this.state.isLoaded
         const id = bookDetails.id
         const title = bookDetails.title
         const shelf = bookDetails.shelf ? bookDetails.shelf : "None"
@@ -38,47 +42,45 @@ class BookDetails extends Component {
 
         return (
             <div>
+            {!isLoaded ? (
+                <div className="loading-icon-container"><img src={LoadingIcon}></img></div>
+            ) : (
                 <div className="book-details-container">
-                    <Link className="close-details" to="/">Close</Link>
-                    <div className="book-details">
-                        <div className="book">
-                            <div className="book-top">
-                                <div 
-                                    className="book-cover" 
-                                    style={{backgroundImage: `url(${cover})` }}>
-                                </div>
-                                < BookSelectButton 
-                                    onUpdate={ onUpdate }
-                                    filteredBook={ bookDetails }
-                                    shelf={shelf}
-                                    key={id}
-                                />
+                <Link className="close-details" to="/">Close</Link>
+                <div className="book-details">
+                    <div className="book">
+                        <div className="book-top">
+                            <div 
+                                className="book-cover" 
+                                style={{backgroundImage: `url(${cover})` }}>
                             </div>
                         </div>
-                        <div className="book-info">
-                            {title && subtitle ? (
-                                <div className="book-title">{title}. {subtitle}</div>
-                            ) : (
-                                <div className="book-title">{title}</div>
-                            )}
-                            {author.length > 1 ? (
-                                <div className="book-authors">{author.join(', ')}</div>
-                            ) : (
-                                <div className="book-authors">{author}</div>
-                            )}
-                            <div className="book-publisher"><span className="rubric">Publisher:</span> {publisher}</div>
-                            <div className="book-date"><span className="rubric">Publish date:</span> {publishedDate}</div>
-                            <div className="book-date"><span className="rubric">Language:</span> {language}</div>
-                            <div className="book-date"><span className="rubric">Pages:</span> {pageCount}</div>
-                            <div className="book-date"><span className="rubric">Category:</span> {categories}</div>
-                            <div className="book-date"><span className="rubric">Average rating:</span> {averageRating}</div>
-                        </div>
-                        <div className="description-container">
-                            <h3 className="details-header">Description</h3>
-                            <div className="book-description">{description}</div>
-                        </div>
+                    </div>
+                    <div className="book-info">
+                        {title && subtitle ? (
+                            <div className="book-title">{title}. {subtitle}</div>
+                        ) : (
+                            <div className="book-title">{title}</div>
+                        )}
+                        {author.length > 1 ? (
+                            <div className="book-authors">{author.join(', ')}</div>
+                        ) : (
+                            <div className="book-authors">{author}</div>
+                        )}
+                        <div className="book-publisher"><span className="rubric">Publisher:</span> {publisher}</div>
+                        <div className="book-date"><span className="rubric">Publish date:</span> {publishedDate}</div>
+                        <div className="book-date"><span className="rubric">Language:</span> {language}</div>
+                        <div className="book-date"><span className="rubric">Pages:</span> {pageCount}</div>
+                        <div className="book-date"><span className="rubric">Category:</span> {categories}</div>
+                        <div className="book-date"><span className="rubric">Average rating:</span> {averageRating}</div>
+                    </div>
+                    <div className="description-container">
+                        <h3 className="details-header">Description</h3>
+                        <div className="book-description">{description}</div>
                     </div>
                 </div>
+            </div>
+            )}
             </div>
         )
     }
