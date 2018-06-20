bb37594d7e9467b6d0bd4f87f96c0c9bc5a15d25
Agata Czerwińska <agata@rubrica.pl>
2018-06-20 22:10:13 +0200

    Group routes with <Switch> Component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from 'react'
 // API
 import * as BooksAPI from './BooksAPI'
 // React Router
-import {Route} from 'react-router-dom'
-import {Link} from 'react-router-dom'
+import {Route, Link, Switch} from 'react-router-dom'
 // Components
 import Books from './Books'
 import Search from './Search'
@@ -35,36 +34,38 @@ class BooksApp extends React.Component {
   }
 
   render() {
-    return (
-      <div className="app"> 
-      <Route exact path="/search" render={(history) => (
-          <Search 
-              books = {this.state.books}
-              onUpdate = {this.updateShelf}
-          />     
-      )}/> 
-      <Route exact path="/" render={(history) => ( 
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <Books 
-                books={this.state.books} 
-                onUpdate={this.updateShelf}
-            />
-            <div className="open-search">
-              <Link to="/search">Add a book</Link >
-            </div>
+      return (
+          <div className="app"> 
+          <Switch>
+              <Route exact path="/search" render={(history) => (
+                  <Search 
+                      books = {this.state.books}
+                      onUpdate = {this.updateShelf}
+                  />     
+              )}/> 
+              <Route exact path="/" render={(history) => ( 
+                  <div className="list-books">
+                    <div className="list-books-title">
+                      <h1>MyReads</h1>
+                    </div>
+                    <Books 
+                        books={this.state.books} 
+                        onUpdate={this.updateShelf}
+                    />
+                    <div className="open-search">
+                      <Link to="/search">Add a book</Link >
+                    </div>
+                  </div>
+              )}/>
+              <Route path="/details/:bookId" render={(props) => (
+                    <BookDetails
+                        id={props.match.params.bookId}
+                        onUpdate={this.updateShelf}
+                    />
+                )}/>
+          </Switch>
           </div>
-      )}/>
-      <Route path="/details/:bookId" render={(props) => (
-            <BookDetails
-                id={props.match.params.bookId}
-                onUpdate={this.updateShelf}
-            />
-        )}/>
-      </div>
-    )
+        )
   }
 }
 
